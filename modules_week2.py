c2509b11febac29ed95843e6d96253950886a5b3
Hakari Bibani <hakary.jalal33@gmail.com>
2025-02-15 22:55:46 +0300

    Update modules_week2.py

--- modules_week2.py
+++ modules_week2.py
@@ -4,19 +4,20 @@ import pandas as pd
 def show():
     # Create 12 tabs with proper comma separation between each tab name.
     tab1, tab2, tab3, tab4, tab5, tab6, tab7, tab8, tab9, tab10, tab11, tab12 = st.tabs([
-        "the Scale up Week",
-        "2.1 Breaking Down Scripts!",
-        "2.2 Quiz",
-        "2.3 Scale up scripts",
-        "2.4 Merging & Reversing",
-        "2.5 G.C for Researchers",
-        "2.6 Case Study",
-        "2.7 Limitations of G.C",
-        "2.8 Google Sheets API",
-        "2.9 GitHub for webapp",
-        "Assignment 3",
-        "Assignment 4"
-    ])
+    "the Scale up Week",
+    "2.1 Breaking Down Scripts!",
+    "2.2 Quiz",
+    "2.3 Scale up scripts",
+    "2.4 Merging & Reversing",
+    "2.5 G.C for Researchers",
+    "2.6 Case Study",
+    "2.7 Limitations of G.C",
+    "2.8 Google Sheets API",
+    "2.9 GitHub for webapp",
+    "Assignment 3",
+    "Assignment 4"
+])
+
 
     with tab1:
         st.header("1.1 Introduction to Python - Recorded Session")
