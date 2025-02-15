370824759e2bf41d1a49532f0d9e0bb997531b4d
Hakari Bibani <hakary.jalal33@gmail.com>
2025-02-15 23:00:41 +0300

    Update modules_week2.py

--- modules_week2.py
+++ modules_week2.py
@@ -20,16 +20,16 @@ def show():
 
 
     with tab1:
-        st.header("1.1 Introduction to Python - Recorded Session")
-        st.video("https://www.youtube.com/watch?v=Scem9sKTtJo")
-        st.subheader("**ChatGPT Prompts**")
-        st.markdown("[Links to an external site](https://chatgpt.com/share/6733c214-7ac4-8004-92f1-227d11b644ff)")
-        st.subheader("**Content**:")
-        st.write(
-            "In this session, we’ll introduce you to the basics of Python and how it can be a powerful tool for enhancing personal impact, "
-            "whether you're looking to automate tasks, analyze data, or create small projects. We will cover foundational topics such as "
-            "setting up your Python environment, understanding Python syntax, and exploring the practical applications of Python in everyday scenarios."
-        )
+    # Display the title in pale orange
+    st.markdown("<h1 style='color: #FFD580;'>Welcome to the Scale up Week</h1>", unsafe_allow_html=True)
+    
+    # Embed the video using the provided iframe HTML code
+    st.markdown(
+        """
+        <iframe width="1110" height="695" src="https://www.youtube.com/embed/OC1J2uZlLdQ" title="December 3, 2024" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+        """,
+        unsafe_allow_html=True
+    )
 
     with tab2:
         st.header("1.2 You made it! Be prepared for your final project")
