f9b83e6e5bffde95c79cd63af96293520dad5a35
Hakari Bibani <hakary.jalal33@gmail.com>
2025-02-15 23:18:43 +0300

    Update modules_week2.py

--- modules_week2.py
+++ modules_week2.py
@@ -21,16 +21,8 @@ def show():
 
     with tab1:
         st.header("Welcome to the Scale up Week")
-        st.video("https://www.youtube.com/watch?v=Scem9sKTtJo")
-        st.subheader("**ChatGPT Prompts**")
-        st.markdown("[Links to an external site](https://chatgpt.com/share/6733c214-7ac4-8004-92f1-227d11b644ff)")
-        st.subheader("**Content**:")
-        st.write(
-            "In this session, we’ll introduce you to the basics of Python and how it can be a powerful tool for enhancing personal impact, "
-            "whether you're looking to automate tasks, analyze data, or create small projects. We will cover foundational topics such as "
-            "setting up your Python environment, understanding Python syntax, and exploring the practical applications of Python in everyday scenarios."
-        )
-
+        st.video("https://youtu.be/OC1J2uZlLdQ")
+        
     with tab2:
         st.header("1.2 You made it! Be prepared for your final project")
         st.video("https://www.youtube.com/watch?v=fD73oMb4NRg")
